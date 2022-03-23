a24540b85a882c874fb80a37a13362ef0c35e42e
Joachim <joachim.stolle@hotmail.de>
2022-03-23 11:25:50 +0100

    Initialer Commit

---
+++ src/main/java/jst/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package jst.helloworld;
+
+import javax.swing.*;
+
+public class HelloWorld {
+
+    public HelloWorld() {
+        JOptionPane.showMessageDialog(null, "Hello World");
+    }
+
+
+}
