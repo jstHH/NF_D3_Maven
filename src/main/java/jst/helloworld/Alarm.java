eac9bf2231ea8d3c659bbce9c92e26d53afff64e
Joachim <joachim.stolle@hotmail.de>
2022-03-23 15:17:33 +0100

    new class Alarm

---
+++ src/main/java/jst/helloworld/Alarm.java
@@ -0,0 +1,19 @@
+package jst.helloworld;
+
+public class Alarm {
+
+    public Alarm() {
+    }
+
+    private int getGuests() {
+
+    }
+
+    private boolean isTooMuch(int count) {
+
+    }
+
+    private void showCount(int count, boolean tooMuch) {
+
+    }
+}
